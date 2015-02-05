ff321abbb1a9b006245ef857cfc953233cdc6ffa
yoshimura <yoshimura@srp.co.jp>
2015-02-06 03:23:36 +0900

    s

--- ThreeDMaze/GameScene.swift
+++ ThreeDMaze/GameScene.swift
@@ -29,29 +29,60 @@ class GameScene: SKScene {
     // MARK: シーン作成
     func createContentScene() {
         
-        var _maps = createMap()
         
         self.backgroundColor = UIColor.whiteColor()
+
+        let _maxX = CGRectGetMaxX(frame)
+        let _maxY = CGRectGetMaxY(frame)
+        
+        let _path = UIBezierPath()
+        _path.moveToPoint(CGPointMake(0, 0))
+        _path.addLineToPoint(CGPointMake(_maxX,0))
+        _path.addLineToPoint(CGPointMake(_maxX,_maxY))
+        _path.addLineToPoint(CGPointMake(0,_maxY))
+        _path.closePath()
+        _path.addLineToPoint(CGPointMake(_maxX, _maxY))
+        _path.moveToPoint(CGPointMake(_maxX, 0))
+        _path.addLineToPoint(CGPointMake(0, _maxY))
+        
+        _path.moveToPoint(CGPointMake(_maxX * 0.1, _maxY * 0.1))
+        _path.addLineToPoint(CGPointMake(_maxX * 0.1, _maxY * (1 - 0.1)))
+        
+        let _sprite = SKShapeNode(path: _path.CGPath)
+        _sprite.userData = [:]
+        _sprite.userData!["hp"] = 4
+        _sprite.userData!["ap"] = 25
+        _sprite.userData!["score"] = 100
+        _sprite.fillColor = SKColor.whiteColor()
+        _sprite.strokeColor = SKColor.blackColor()
+        _sprite.name = "enemy"
+        _sprite.zPosition = 100
+
+        addChild(_sprite)
+        
+        
+        var _maps = createMap()
+        
         // let _map:[Int]=[]
         var _x = 0, _y = 0
         let _max=15
         
         //for _z in 0..<6 {
-            for _y in 0..<_max {
-                for _x in 0..<_max {
-                    
-                    if _maps[_x+_y*_max+_max*_max*14] == 0 {
-                        continue
-                    }
-                    
-                    let _shape =  SKShapeNode(rect: CGRectMake(0, 0, 5, 5));
-                    _shape.strokeColor = SKColor.blackColor()
-                    _shape.fillColor=SKColor.blackColor()
-                    _shape.position = CGPointMake(CGFloat(_x*5+100), CGFloat(_y*5+100*1))
-                    _shape.name = "w" + "\(_x+_y*_max)"
-                    self.addChild(_shape)
+        for _y in 0..<_max {
+            for _x in 0..<_max {
+                
+                if _maps[_x+_y*_max+_max*_max*14] == 0 {
+                    continue
                 }
+                
+                let _shape =  SKShapeNode(rect: CGRectMake(0, 0, 5, 5));
+                _shape.strokeColor = SKColor.blackColor()
+                _shape.fillColor=SKColor.blackColor()
+                _shape.position = CGPointMake(CGFloat(_x*5+100), CGFloat(_y*5+100*1))
+                _shape.name = "w" + "\(_x+_y*_max)"
+                //self.addChild(_shape)
             }
+        }
         //}
     }
     
@@ -175,7 +206,120 @@ class GameScene: SKScene {
         return _map
     }
     
-    func getWalls(x:Int,y:Int,z:Int,vec:Int,map:[Int])->[Int] {
+    enum Direction {
+        case N
+        case W
+        case S
+        case E
+        case C
+        case F
+        
+        func xyz()->[Int] {
+            switch self {
+            case .N:
+                return [0,1,0];
+            case .W:
+                return [1,0,0];
+            case .S:
+                return [0,-1,0];
+            case .E:
+                return [-1,0,0];
+            case .C:
+                return [0,0,1];
+            case .F:
+                return [0,0,-1];
+            }
+        }
+    }
+    
+    func calcXyz(front:Direction, head:Direction, x:Int, y:Int, z:Int, xx:Int, yy:Int, zz:Int)->[Int] {
+        
+        switch front {
+        case .N:
+            switch head {
+            case .C:
+                return [x+xx,y+yy,z+zz]
+            case .W:
+                return [z+(-1*xx),y+yy,x+zz]
+            case .F:
+                return [x+(-1*xx), y+yy,z+(-1*zz)]
+            case .E:
+                return [z+xx,y+yy,x+(-1*zz)]
+            default:
+                return [x,y,z]
+            }
+        case .W:
+            switch head {
+            case .C:
+                return [y+(-1*xx),x+yy,z+zz]
+            case .S:
+                return [z+(-1*xx),x+yy,y+zz]
+            case .F:
+                return [y+xx,x+yy,z+(-1*zz)]
+            case .N:
+                return [z+xx,x+yy,y+(-1*zz)]
+            default:
+                return [x,y,z]
+            }
+        case .S:
+            switch head {
+            case .C:
+                return [x+(-1*xx),y+(-1*yy),z+zz]
+            case .W:
+                return [z+xx,y+(-1*yy),x+zz]
+            case .F:
+                return [x+xx, y+(-1*yy),z+(-1*zz)]
+            case .E:
+                return [z+(-1*xx), y+(-1*yy),x+(-1*zz)]
+            default:
+                return [x,y,z]
+            }
+        case .E:
+            switch head {
+            case .C:
+                return [y+xx,x+(-1*yy),z+zz]
+            case .S:
+                return [z+xx,x+(-1*yy),y+(-1*zz)]
+            case .F:
+                return [y+(-1*xx),x+(-1*yy),z+(-1*zz)]
+            case .N:
+                return [z+(-1*xx),x+(-1*yy),y+zz]
+            default:
+                return [x,y,z]
+            }
+        case .C:
+            switch head {
+            case .N:
+                return [x+(-1*xx),z+yy,y+zz]
+            case .W:
+                return [y+xx,z+yy,x+zz]
+            case .S:
+                return [x+xx,z+yy,y+(-1*zz)]
+            case .E:
+                return [y+(-1*xx),z+yy,x+(-1*zz)]
+            default:
+                return [x,y,z]
+            }
+        case .F:
+            switch head {
+            case .N:
+                return [x+xx,z+(-1*yy),y+zz]
+            case .W:
+                return [y+(-1*xx),z+(-1*yy),x+zz]
+            case .S:
+                return [x+(-1*xx),z+(-1*yy),y+(-1*zz)]
+            case .E:
+                return [y+xx,z+(-1*yy),x+(-1*zz)]
+            default:
+                return [x,y,z]
+            }
+        default:
+            return [x,y,z]
+        }
+                
+    }
+    
+    func getWalls(front:Direction, head:Direction, x:Int,y:Int,z:Int, map:[Int])->[Int] {
         
         let _max = 15
         func _func(x1:Int,y1:Int,z1:Int)->Int {
@@ -187,26 +331,25 @@ class GameScene: SKScene {
         }
         
         var _res:[Int]=[Int](count: 3*5, repeatedValue: 1)
-    
+        
         _res=[]
         
-        // 北
-        if vec==0 {
+        for _zz in [-1,0,1] {
             for _yy in [0,1,2,3,4] {
                 for _xx in [-1,0,1] {
-                    if _func(x+_xx,y+_yy,z) == 0 {
+                    let _xyz = self.calcXyz(front, head: head, x: x, y: y, z: z, xx: _xx, yy:_yy, zz:_zz)
+                    
+                    if _func(_xyz[0], _xyz[1], _xyz[2]) == 0 {
                         _res += [0]
                     } else {
                         _res += [1]
                     }
                 }
             }
-            return []
         }
         
-        // 回転による反転
         
-        return []
+        return _res
     }
     
 }
